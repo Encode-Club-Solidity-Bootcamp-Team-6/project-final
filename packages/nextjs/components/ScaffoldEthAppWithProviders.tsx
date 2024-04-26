4b6d42d0f0f8408074e645ae7b76ebaa2e67804e
tomasgrusz <grusztomas0@gmail.com>
2024-04-26 20:56:02 +0200

    feat: create /umbrella page, add components, cleanup

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
 import { WagmiProvider } from "wagmi";
 import { Footer } from "~~/components/Footer";
@@ -45,8 +44,7 @@ export const queryClient = new QueryClient({
 });
 
 export const ScaffoldEthAppWithProviders = ({ children }: { children: React.ReactNode }) => {
-  const { resolvedTheme } = useTheme();
-  const isDarkMode = resolvedTheme === "dark";
+  const isDarkMode = true;
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
