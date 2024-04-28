ec63a5062b9b578a3e603339de37958bd45ef17e
reverendlovejoy43782 <alias23423@mailbox.org>
2024-04-28 17:43:32 +0200

    removed testfile for testscripts

--- packages/hardhat/test/Test.ts
+++
@@ -1,8 +0,0 @@
-// Add this to your fetchAccounts.ts or create a new script file.
-console.log("Testing ethers utils:");
-try {
-    const ethValue = ethers.utils.parseEther("1.0");
-    console.log(`1.0 ETH in Wei: ${ethValue.toString()}`);
-} catch (error) {
-    console.error("Error using ethers.utils.parseEther:", error);
-}
